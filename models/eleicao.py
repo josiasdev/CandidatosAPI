fe3d9bbc3615c419f1a0e6da69fc447b7b570e65
Gabriel Brito <34654875+gabriel-bri@users.noreply.github.com>
2025-03-04 16:25:23 -0300

    fix: Fix wrong name model and add others details

--- models/eleicao.py
+++ models/eleicao.py
@@ -4,11 +4,13 @@ from datetime import datetime
 # consulta_cand_2024.csv + consulta_vagas_2024.csv
 class Eleicao(BaseModel):
     #Eleicao
-    cd_eleicao: str # id eleicao
+    # Pegar somente 1 de cada código
+    cd_eleicao: str # id eleicao, único por eleição e por turno
     ds_eleicao: str # descricao eleicao
     dt_eleicao: datetime # data eleicao
     ano_eleicao: int
-    cd_tipo_elicao: str
+    cd_tipo_eleicao: str
     nm_tipo_eleicao: str
     tp_abrangencia: str # tipo abrangencia eleicao
-    dt_posse: datetime # from vagas - NAO TEM no Candidato
+    nr_turno: str
+    dt_posse: datetime # from vagas - NAO TEM no Candidato, tá em vagas
