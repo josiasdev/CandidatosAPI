c346f919f8dffb3b37155682be4bb4f2282a976d
Gabriel Brito <34654875+gabriel-bri@users.noreply.github.com>
2025-03-05 15:39:27 -0300

    refactor: Change model data

--- models/eleicao.py
+++ models/eleicao.py
@@ -2,15 +2,15 @@ from pydantic import BaseModel
 from datetime import datetime
 
 # consulta_cand_2024.csv + consulta_vagas_2024.csv
-class Eleicao(BaseModel):
+class EleicaoBase(BaseModel):
     #Eleicao
     # Pegar somente 1 de cada código
     cd_eleicao: str # id eleicao, único por eleição e por turno
     ds_eleicao: str # descricao eleicao
     dt_eleicao: datetime # data eleicao
-    ano_eleicao: int
+    ano_eleicao: str
     cd_tipo_eleicao: str
     nm_tipo_eleicao: str
     tp_abrangencia: str # tipo abrangencia eleicao
     nr_turno: str
-    dt_posse: datetime # from vagas - NAO TEM no Candidato, tá em vagas
+    qt_vaga: int # from vagas - NAO TEM no Candidato, tá em vagas
