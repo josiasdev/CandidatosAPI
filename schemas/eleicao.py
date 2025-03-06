897307e25da9c07f74dc62d40f8d2920b90b0cc0
Gabriel Brito <34654875+gabriel-bri@users.noreply.github.com>
2025-03-06 11:21:42 -0300

    refactor: Neew schema to eleicao

--- schemas/eleicao.py
+++ schemas/eleicao.py
@@ -1,15 +1,25 @@
-from models.eleicao import EleicaoBase
+from models.eleicao import EleicaoCreate, EleicaoPublic
 from datetime import datetime
 
-def eleicao_entity(entity: dict) -> EleicaoBase:
-    return EleicaoBase(
-        cd_eleicao=str(entity['CD_ELEICAO']),  # Transformando para string
-        ds_eleicao=entity['DS_ELEICAO'],  # Descrição da eleição
-        dt_eleicao=datetime.strptime(entity['DT_ELEICAO'], '%d/%m/%Y'),  # Convertendo string para datetime
-        ano_eleicao=str(entity['ANO_ELEICAO']),  # Convertendo para string
-        cd_tipo_eleicao=str(entity['CD_TIPO_ELEICAO']),  # Convertendo para string
-        nm_tipo_eleicao=entity['NM_TIPO_ELEICAO'],  # Nome do tipo de eleição
-        tp_abrangencia=entity['TP_ABRANGENCIA'],  # Tipo de abrangência da eleição
-        nr_turno=str(entity['NR_TURNO']),  # Convertendo para string
-        qt_vaga=entity['QT_VAGA'],  # Quantidade de vagas
-    )
+def eleicao_entity(entity: dict) -> EleicaoCreate:
+    return {
+        "cd_eleicao" : entity['cd_eleicao'],  # Transformando para string
+        "ds_eleicao" : entity['ds_eleicao'],  # Descrição da eleição
+        "dt_eleicao" : entity['dt_eleicao'],  # Convertendo string para datetime
+        "ano_eleicao" : entity['ano_eleicao'],  # Convertendo para string
+        "cd_tipo_eleicao" :entity['cd_tipo_eleicao'],  # Convertendo para string
+        "nm_tipo_eleicao" : entity['nm_tipo_eleicao'],  # Nome do tipo de eleição
+        "tp_abrangencia" : entity['tp_abrangencia'],  # Tipo de abrangência da eleição
+        "nr_turno" : entity['nr_turno'],  # Convertendo para string
+    }
+
+def eleicao_entity_from_db(entity: dict) -> EleicaoPublic:
+    eleicao = {
+        'id': str(entity['_id']),
+        **eleicao_entity(entity)
+    }
+    
+    return eleicao
+
+def eleicao_entities_from_db(entities: list) -> list[EleicaoPublic]:
+    return [eleicao_entity_from_db(entity) for entity in entities]
