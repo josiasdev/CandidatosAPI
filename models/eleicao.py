f96bf4e693853d89ce713b7c23baee1ab729a609
Gabriel Brito <34654875+gabriel-bri@users.noreply.github.com>
2025-03-06 11:20:59 -0300

    refactor: New model to eleicao

--- models/eleicao.py
+++ models/eleicao.py
@@ -5,12 +5,33 @@ from datetime import datetime
 class EleicaoBase(BaseModel):
     #Eleicao
     # Pegar somente 1 de cada código
-    cd_eleicao: str # id eleicao, único por eleição e por turno
     ds_eleicao: str # descricao eleicao
     dt_eleicao: datetime # data eleicao
-    ano_eleicao: str
-    cd_tipo_eleicao: str
+    ano_eleicao: int
+    cd_tipo_eleicao: int
     nm_tipo_eleicao: str
     tp_abrangencia: str # tipo abrangencia eleicao
-    nr_turno: str
-    qt_vaga: int # from vagas - NAO TEM no Candidato, tá em vagas
+    nr_turno: int
+
+class EleicaoPublicMixin(BaseModel):
+    id: str # ID - primary-key
+    cd_eleicao: int # id eleicao, único por eleição e por turno
+        
+class EleicaoCreateMixin(BaseModel):
+    cd_eleicao: int # id eleicao, único por eleição e por turno
+
+class EleicaoPublic(EleicaoBase, EleicaoPublicMixin):
+    pass
+
+class EleicaoCreate(EleicaoBase, EleicaoCreateMixin):
+    pass
+
+class EleicaoUpdate(BaseModel):
+    ds_eleicao: str | None = None # descricao eleicao
+    dt_eleicao: datetime | None = None # data eleicao
+    ano_eleicao: int | None = None
+    cd_tipo_eleicao: int | None = None
+    nm_tipo_eleicao: str | None = None
+    tp_abrangencia: str | None = None # tipo abrangencia eleicao
+    nr_turno: int | None = None
+    qt_vaga: int | None = None # from vagas - NAO TEM no Candidato, tá em vagas
